a7b9bc2e8ad80fc3a03c1e5e6d8e0685441c40a6
kiran <Kiran.Kavali@agrometrics.com>
2021-10-19 16:11:23 +0530

    added

--- intern_task/dash/views.py
+++ intern_task/dash/views.py
@@ -1,8 +1,11 @@
+
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
-def func(x, n):
+def func(x, n): 
+    """ recursive function to calculate 1/x^i"""     
     if n == 1:
         return 1 / x
     else:
@@ -18,9 +21,14 @@ def home(request):
 @login_required()
 def calculate(request):
     if request.method == 'POST':
+        
+        if((request.POST.get('x_id') == "") or (request.POST.get('n_id') == "")):
+            
+            messages.error(request,'Please enter both X and N values !!!')
+            return redirect('calc')
         x = int(request.POST.get('x_id'))
         n = int(request.POST.get('n_id'))
-        print(x, n)
+        
         res = func(x, n)
         return render(request, 'dash/calc.html', {'res': res})
     return render(request, 'dash/calc.html')
